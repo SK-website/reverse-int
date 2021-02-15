fcffd899105351fb461061af03c333ab4bddb9ba
SK-website <vnali@tutanota.com>
2021-02-15 20:43:18 +0200

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    if (n < 0) {
+        n = n * -1;
+    }
+    a = String(n).split("").reverse().join("");
+    return +a;
+};
